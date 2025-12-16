9290cf2a2d58bcb2cf55c699d726a1fb7593537e
Sawariya-Dixit <sawariyadixit085@gmail.com>
2025-12-16 21:42:04 +0530

    fix: enable cors for vercel frontend

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));     // <-- ADD THIS
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000","http://127.0.0.1:5173",];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000","http://127.0.0.1:5173", "https://study-notion-one-lac.vercel.app"];
 
 app.use(
   cors({
